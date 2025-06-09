e39edbf8d2c8f53a68db75c9e6e4e8d04301f9dc
Geoffdl <geoffroydelorme@gmail.com>
2025-06-09 15:04:56 +0200

    Initial commit
    Signed-off-by: Geoffdl <geoffroydelorme@gmail.com>

---
+++ src/main/java/fr/diginamic/qualiair/QualiAirApplication.java
@@ -0,0 +1,14 @@
+package fr.diginamic.qualiair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualiAirApplication
+{
+    
+    public static void main(String[] args)
+    {
+        SpringApplication.run(QualiAirApplication.class, args);
+    }
+}
---
+++ src/test/java/fr/diginamic/qualiair/QualiAirApplicationTests.java
@@ -0,0 +1,14 @@
+package fr.diginamic.qualiair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualiAirApplicationTests
+{
+    
+    @Test
+    void contextLoads()
+    {
+    }
+}
