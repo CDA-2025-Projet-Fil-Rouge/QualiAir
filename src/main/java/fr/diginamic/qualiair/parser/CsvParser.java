bb5218aed9a33df88cc95abfbb56df856ba5bfdb
Geoffdl <geoffroydelorme@gmail.com>
2025-06-12 13:06:33 +0200

    Subject : Parsing cities from file data sources
    Impl : IO file reader impl
    Signed-off-by: Geoffdl <geoffroydelorme@gmail.com>

--- src/main/java/fr/diginamic/qualiair/parser/CsvParser.java
+++ src/main/java/fr/diginamic/qualiair/parser/CsvParser.java
@@ -1,6 +1,20 @@
 package fr.diginamic.qualiair.parser;
 
-public class CsvParser
-{
-    private String fileName;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+@Component
+public class CsvParser {
+
+
+    public List<String> parseFile(String filePath) throws IOException {
+        Path path = Paths.get(filePath);
+        return Files.readAllLines(path, StandardCharsets.UTF_8);
+    }
 }
