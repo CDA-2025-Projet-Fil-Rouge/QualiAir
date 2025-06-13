fdd75d7ffdcebd0bf11ede8417c75d61c6c66ad7
Geoffdl <geoffroydelorme@gmail.com>
2025-06-13 09:17:08 +0200

    Subject : Parsing cities from file data sources
    Impl :
    
    Status :
    
    notable issue :
    
    Signed-off-by: Geoffdl <geoffroydelorme@gmail.com>

---
+++ src/main/java/fr/diginamic/qualiair/repository/MesureRepository.java
@@ -0,0 +1,7 @@
+package fr.diginamic.qualiair.repository;
+
+import fr.diginamic.qualiair.entity.Mesure;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MesureRepository extends JpaRepository<Mesure, Long> {
+}
