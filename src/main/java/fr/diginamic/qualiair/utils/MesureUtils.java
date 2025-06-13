6e40d0a9b4ed015e08c00589d7d8d1dab82c7fe6
Geoffdl <geoffroydelorme@gmail.com>
2025-06-13 09:17:45 +0200

    Subject : Parsing cities from file data sources
    Impl : key generator for hashmap cache
    Status :
    
    notable issue :
    
    Signed-off-by: Geoffdl <geoffroydelorme@gmail.com>

--- src/main/java/fr/diginamic/qualiair/utils/MesureUtils.java
+++ src/main/java/fr/diginamic/qualiair/utils/MesureUtils.java
@@ -1,5 +1,6 @@
 package fr.diginamic.qualiair.utils;
 
+import fr.diginamic.qualiair.entity.Mesure;
 import fr.diginamic.qualiair.exception.ParsedDataException;
 
 public class MesureUtils {
@@ -9,5 +10,15 @@ public class MesureUtils {
             throw new ParsedDataException("Population must be a valid number");
         }
         return Integer.parseInt(string.trim());
+    }
+
+    public static String toKey(String nom, double lat, double lon) {
+        String coordKey = CoordonneeUtils.toKey(lat, lon);
+        return nom + "_" + coordKey;
+    }
+
+    public static String toKey(Mesure mesure) {
+        String coordKey = CoordonneeUtils.toKey(mesure.getCoordonnee().getLatitude(), mesure.getCoordonnee().getLongitude());
+        return mesure.getNom() + "_" + coordKey;
     }
 }
