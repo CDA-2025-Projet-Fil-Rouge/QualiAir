dad7697079e9007388dcce7cb771736d70163782
Geoffdl <geoffroydelorme@gmail.com>
2025-07-18 11:35:54 +0200

    feat(docker): docker wip
    TODO:
    
    Signed-off-by: Geoffdl <geoffroydelorme@gmail.com>

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:latest
+LABEL authors="Geoff"
+
+ENTRYPOINT ["top", "-b"]
--- src/main/java/fr/diginamic/qualiair/config/SecurityConfig.java
+++ src/main/java/fr/diginamic/qualiair/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://92.88.240.121:4200"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
